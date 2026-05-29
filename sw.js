114403326ee669d447d2a21d9c46e32cd2ffa9af
George Grimbilas <george@grimbilas.com>
2026-05-29 18:29:24 -0400

    Add contact details, logos, search, and reorganized detail view to CRM

--- sw.js
+++ sw.js
@@ -2,4 +2,12 @@ const C='evcrm-2026-05-29';
 const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('fetch',e=>{
+  const u=e.request.url;
+  if(u.indexOf('/att/')>=0){
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{
+      const cp=resp.clone();caches.open(C).then(c=>c.put(e.request,cp));return resp;})));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
